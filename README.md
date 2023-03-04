148d2bf18e5e11d9a38074d373eb44c0845f2115
Tyson Liddell <tysonliddell@hotmail.com>
2023-03-04 19:47:59 +0000

    Initial commit: Hello ppm image!

---
+++ README.md
@@ -0,0 +1,3 @@
+# ray-tracing-1
+Code following Peter Shirley's *Ray Tracing in One Weekend*. Book can be
+found [here](https://raytracing.github.io/books/RayTracingInOneWeekend.html).
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // Image
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width-1) as f64;
+            let g = j as f64 / (image_height-1) as f64;
+            let b = 0.25;
+
+            let r = (255.999 * r) as i32;
+            let g = (255.999 * g) as i32;
+            let b = (255.999 * b) as i32;
+
+            println!("{r} {g} {b}");
+        }   
+    }
+
+}
