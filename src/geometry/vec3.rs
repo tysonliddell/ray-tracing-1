5b1962ae3743f55751b683e81357c22af1edc1b6
Tyson Liddell <tysonliddell@hotmail.com>
2023-03-05 12:37:45 +0000

    Add more Vec3 functionality.

--- src/geometry/vec3.rs
+++ src/geometry/vec3.rs
@@ -9,6 +9,8 @@ pub struct Vec3 {
     pub z: f64,
 }
 
+const VEC3_DIM: usize = 3;
+
 impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
@@ -23,6 +25,30 @@ impl Vec3 {
     pub fn length_squared(&self) -> f64 {
         let Self { x, y, z } = self;
         x * x + y * y + z * z
+    }
+
+    /// Get the dot product with another vectors.
+    pub fn dot(&self, other: &Self) -> f64 {
+        (0..VEC3_DIM).map(|i| self[i] * other[i]).sum()
+    }
+
+    /// Get the cross product with another vector.
+    pub fn cross(&self, other: &Self) -> Self {
+        Self::new(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+    }
+
+    /// Get the unit vector in the direction given.
+    pub fn unit_vector(&self) -> Self {
+        *self / self.length()
+    }
+
+    /// Normalize the current vector to have unit length.
+    pub fn normalize(&mut self) {
+        *self /= self.length();
     }
 }
 
@@ -56,11 +82,47 @@ impl ops::AddAssign for Vec3 {
     }
 }
 
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
 impl ops::Mul<f64> for Vec3 {
     type Output = Self;
 
     fn mul(self, rhs: f64) -> Self::Output {
         Self::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl ops::Mul<Self> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
+
+impl ops::MulAssign<Self> for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
     }
 }
 
@@ -116,6 +178,9 @@ impl ops::IndexMut<usize> for Vec3 {
 mod test {
     use super::Vec3;
 
+    // A pythagorean quadruple (a,b,c,d) satisfies a*a + b*b + c*c == d*d
+    const PYTHAGOREAN_QUADRUPLE: (f64, f64, f64, f64) = (4.0, 13.0, 16.0, 21.0);
+
     #[test]
     fn default() {
         assert_eq!(Vec3::new(0.0, 0.0, 0.0), Vec3::default())
@@ -123,8 +188,9 @@ mod test {
 
     #[test]
     fn length() {
-        let v = Vec3::new(1.0, 2.0, 3.0);
-        let d_sqrd = 1.0 * 1.0 + 2.0 * 2.0 + 3.0 * 3.0;
+        let (x, y, z, d) = PYTHAGOREAN_QUADRUPLE;
+        let v = Vec3::new(x, y, z);
+        let d_sqrd = 21.0 * 21.0;
         assert_eq!(d_sqrd, v.length_squared());
         assert_eq!(d_sqrd.sqrt(), v.length());
     }
@@ -139,37 +205,89 @@ mod test {
     fn add() {
         let v1 = Vec3::new(1.0, 2.0, 3.0);
         let v2 = Vec3::new(4.0, 5.0, 6.0);
-        let mut v3 = v1 + v2;
 
-        assert_eq!(Vec3::new(5.0, 7.0, 9.0), v3);
+        assert_eq!(Vec3::new(5.0, 7.0, 9.0), v1 + v2);
 
-        v3 += v1;
-        assert_eq!(Vec3::new(6.0, 9.0, 12.0), v3);
+        let mut v1 = v1;
+        v1 += v2;
+        assert_eq!(Vec3::new(5.0, 7.0, 9.0), v1);
     }
 
     #[test]
-    fn mul() {
-        let mut v1 = Vec3::new(1.0, 2.0, 3.0);
-        assert_eq!(Vec3::new(3.0, 6.0, 9.0), v1 * 3.0);
+    fn subtract() {
+        let v1 = Vec3::new(2.0, 2.0, 2.0);
+        let v2 = Vec3::new(3.0, 2.0, 1.0);
+        assert_eq!(Vec3::new(-1.0, 0.0, 1.0), v1 - v2);
 
-        v1 *= 2.0;
-        assert_eq!(Vec3::new(2.0, 4.0, 6.0), v1);
+        let mut v1 = v1;
+        v1 -= v2;
+        assert_eq!(Vec3::new(-1.0, 0.0, 1.0), v1);
     }
 
     #[test]
-    fn div() {
-        let mut v1 = Vec3::new(6.0, 12.0, 18.0);
-        assert_eq!(Vec3::new(3.0, 6.0, 9.0), v1 / 2.0);
+    fn mul_pointwise() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(2.0, 3.0, 4.0);
+        assert_eq!(Vec3::new(2.0, 6.0, 12.0), v1 * v2);
 
-        v1 /= 3.0;
-        assert_eq!(Vec3::new(2.0, 4.0, 6.0), v1);
+        let mut v1 = v1;
+        v1 *= v2;
+        assert_eq!(Vec3::new(2.0, 6.0, 12.0), v1);
+    }
+
+    #[test]
+    fn mul_with_scalar() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(Vec3::new(3.0, 6.0, 9.0), v * 3.0);
+        assert_eq!(Vec3::new(3.0, 6.0, 9.0), 3.0 * v);
+
+        let mut v = v;
+        v *= 2.0;
+        assert_eq!(Vec3::new(2.0, 4.0, 6.0), v);
+    }
+
+    #[test]
+    fn div_with_scalar() {
+        let v = Vec3::new(6.0, 12.0, 18.0);
+        assert_eq!(Vec3::new(3.0, 6.0, 9.0), v / 2.0);
+
+        let mut v = v;
+        v /= 3.0;
+        assert_eq!(Vec3::new(2.0, 4.0, 6.0), v);
+    }
+
+    #[test]
+    fn dot() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(2.0, 3.0, 4.0);
+        assert_eq!(2.0 + 6.0 + 12.0, v1.dot(&v2));
+    }
+
+    #[test]
+    fn cross() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(2.0, 3.0, 4.0);
+        assert_eq!(Vec3::new(-1.0, 2.0, -1.0), v1.cross(&v2));
+        assert_eq!(Vec3::new(1.0, -2.0, 1.0), v2.cross(&v1));
+    }
+
+    #[test]
+    fn unit_vector() {
+        let (x, y, z, d) = PYTHAGOREAN_QUADRUPLE;
+        let v = Vec3::new(x, y, z);
+        assert_eq!(Vec3::new(x / d, y / d, z / d), v.unit_vector());
+
+        let mut v = v;
+        v.normalize();
+        assert_eq!(Vec3::new(x / d, y / d, z / d), v);
     }
 
     #[test]
     fn index() {
-        let mut v = Vec3::new(1.0, 2.0, 3.0);
+        let v = Vec3::new(1.0, 2.0, 3.0);
         assert_eq!((1.0, 2.0, 3.0), (v[0], v[1], v[2]));
 
+        let mut v = v;
         for i in 0..3 {
             v[i] *= 2.0;
         }
