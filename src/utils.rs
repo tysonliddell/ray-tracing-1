2a51124280281c83c5b15a2a6d6841c610f77bb1
Tyson Liddell <tysonliddell@hotmail.com>
2023-03-16 17:50:51 +0000

    Simplify gamma 2 correction function.

--- src/utils.rs
+++ src/utils.rs
@@ -4,14 +4,15 @@ pub(crate) mod rand;
 
 /// Apply gamma 2 correction to an image.
 pub fn correct_gamma(pixels: &mut ImagePixels) {
-    // on second thoughts, it might have been a better idea to leave all color values
+    // on second thought, it might have been a better idea to leave all color values
     // as floats internally, and only convert to the [0-255] range at the end.
     // ¯\_(ツ)_/¯
     for row in pixels {
         for color in row {
-            color.red = ((((color.red as f64) / 255.0).sqrt()) * 255.0) as u8;
-            color.green = ((((color.green as f64) / 255.0).sqrt()) * 255.0) as u8;
-            color.blue = ((((color.blue as f64) / 255.0).sqrt()) * 255.0) as u8;
+            // (x/255)^0.5 * 255 = (x * 255)*0.5
+            color.red = (color.red as f64 * 255.0).sqrt() as u8;
+            color.green = (color.green as f64 * 255.0).sqrt() as u8;
+            color.blue = (color.blue as f64 * 255.0).sqrt() as u8;
         }
     }
 }
